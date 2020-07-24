cbd335c883f886b6c6d9a8412cdc5a369c29a1cd
yuto <yuhto.1027@gmail.com>
2020-07-24 12:16:48 +0900

    question

---
+++ Assets/Scripts/CubeCollision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeCollision : MonoBehaviour
+{
+    public Text scoreText;
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        SetCountText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Straged"))
+        {
+            score++;
+        }
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+        scoreText.text = "Count: " + score.ToString();
+    }
+}
